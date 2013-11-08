4991d9b7b796c0aff402385c69e17e304df80d6e
kordano <konrad.kuehne@rocketmail.com>
2013-11-08 20:00:05 +0100

    migrate to httpkit

--- src/clj/kubrick/ws_server.clj
+++ src/clj/kubrick/ws_server.clj
@@ -1,13 +1,2 @@
 (ns kubrick.ws-server
-  (:use [lamina.core]
-        [aleph.http]))
-
-(def broadcast-channel (channel))
-
-(defn chat-handler [ch handshake]
-  (receive ch
-    (fn [input]
-      (siphon (map* #(str input ": " %) ch) broadcast-channel)
-      (siphon broadcast-channel ch))))
-
-(start-http-server chat-handler {:port 8008 :websocket true})
+  (:use org.httpkit.server))
