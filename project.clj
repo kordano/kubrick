55ee9ea69b4025c775b179fbd7f5b6fe3e949905
kordano <konrad.kuehne@rocketmail.com>
2013-11-23 19:12:01 +0100

    remove jetty-async, add cheshire for json testing

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1934"]
                  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+                 [org.clojure/core.match "0.2.0"]
                  ;;[org.bodil/cljs-noderepl "0.1.10"]
                  [ring "1.2.0"]
                  [compojure "1.1.5"]
@@ -18,11 +19,11 @@
                  [prismatic/dommy "0.1.1"]
                  [hiccups "0.2.0"]
                  [http-kit "2.1.12"]
-                 ]
+                 [cheshire "5.2.0"]]
   :profiles {:dev {:repl-options {:init-ns kubrick.server}
                    :plugins [[lein-cljsbuild "0.3.2"]
                              [com.cemerick/austin "0.1.1"]]
                    :cljsbuild {:builds [{:source-paths ["src/cljs"]
                                          :compiler {:output-to "resources/public/js/main.js"
-                                                    :optimizations :simple
+                                                    :optimizations :none
                                                     :pretty-print true}}]}}})
