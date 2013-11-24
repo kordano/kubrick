337d6163c453119f785229420d23efe97e4a367c
kordano <konrad.kuehne@rocketmail.com>
2013-11-24 21:37:04 +0100

    handle different request types in websocket

--- src/clj/kubrick/server.clj
+++ src/clj/kubrick/server.clj
@@ -6,13 +6,37 @@
             [compojure.core :refer (GET defroutes)]
             [ring.adapter.jetty :refer [run-jetty]]
             [clojure.java.io :as io]
-            [ring.middleware.reload :as reload]
             [cheshire.core :refer :all]
+            [kubrick.datalog :as db :refer [get-couchdb-entries put!]]
             [org.httpkit.server :refer [with-channel on-close on-receive run-server send!]]))
 
 
-                                        ; ring server
 
+(defn destructure-request [{type :type data :data}]
+  (cond
+   (= type "query") (db/get-couchdb-entries)
+   (= type "insertion") (db/put! data)
+   :else "WRONG REQUEST"))
+
+
+                                        ; websocket server
+(defn handler [request]
+  (with-channel request channel
+    (on-close channel (fn [status] (println "channel closed: " status)))
+    (on-receive channel (fn [data] ;; echo it back
+                          (do
+                            (println (str "data received: " (str (read-string data))))
+                            (send! channel (str (destructure-request (read-string data)))))))))
+
+
+(defn run-ws []
+  (defonce ^:private ws-server
+    (run-server handler {:port 9090}))
+  ws-server)
+
+
+
+                                        ; ring server
 (enlive/deftemplate page
   (io/resource "public/index.html")
   []
@@ -31,23 +55,5 @@
   server)
 
 
-                                        ; websocket server
-
-(defn handler [request]
-  (with-channel request channel
-    (on-close channel (fn [status] (println "channel closed: " status)))
-    (on-receive channel (fn [data] ;; echo it back
-                          (do
-                            (println (type (read-string data)))
-                            (send! channel (str (read-string data))))))))
-
-
-(defn run-ws []
-  (defonce ^:private ws-server
-    (run-server handler {:port 9090}))
-  ws-server)
-
 #_(run-ws)
 #_(run)
-
-#_(reload/wrap-reload #'handler)
